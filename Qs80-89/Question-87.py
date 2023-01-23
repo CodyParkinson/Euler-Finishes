1f3b86c8238ec6e746ba4ce1d94188fcee94d629
CodyParkinson <108588077+CodyParkinson@users.noreply.github.com>
2023-01-23 09:19:04 -0700

    Update Question-87.py

--- Qs80-89/Question-87.py
+++ Qs80-89/Question-87.py
@@ -1,5 +1,5 @@
 '''
-01/20/2022
+01/20/2023
 
 Q: The smallest number expressible as the sum of a prime square, prime cube, and prime 
 fourth power is 28. In fact, there are exactly four numbers below fifty that can be expressed in such a way:
